33431ccee2fe9ff00396f155451de8d9e5d97c99
Dmitriy Pak <fly2sky7777@mail.ru>
2017-10-24 21:23:02 +0300

    получена model из потока

--- src/main/java/ru/bmstu/cryptotext/objects/UploadedFileService.java
+++ src/main/java/ru/bmstu/cryptotext/objects/UploadedFileService.java
@@ -3,6 +3,9 @@ package ru.bmstu.cryptotext.objects;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.KeyStore.Entry;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +19,13 @@ public class UploadedFileService {
 //		String rootPath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
 //		String rootPath2 = request.getSession().getServletContext().getRealPath("/");
 		if (file.getFile()!=null) {
+			Map<?,?>map = new LinkedHashMap<>();
+			map = context.getFlowScope().asMap();
+			for(Map.Entry<?, ?> item:map.entrySet()) {
+				UploadedFile uploadedFile = (UploadedFile) item.getValue();
+				System.out.println("$$$ полученный файл " + uploadedFile.getFile().getOriginalFilename());
+				System.out.println(item.getKey());
+			}
 			return "success";
 		} else {
 			return "failed";
